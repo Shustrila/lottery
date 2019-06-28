2d4a96b37c921f7866177e672937bdce84ba2fdd
AppVeyor <ci@appveyor.com>
2019-06-28 08:45:01 +0000

    089a2e3

---
+++ precache-manifest.bcc2896b74aeacb1925c2a346173f575.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "33c24734eceb8186911bd7261b5d32a3",
+    "url": "/lottery/index.html"
+  },
+  {
+    "revision": "85ce4ccb63a194485a4a",
+    "url": "/lottery/static/css/main.c1d2ff23.chunk.css"
+  },
+  {
+    "revision": "664fcb3371389a6326ed",
+    "url": "/lottery/static/js/2.25312ee8.chunk.js"
+  },
+  {
+    "revision": "85ce4ccb63a194485a4a",
+    "url": "/lottery/static/js/main.4ad2830e.chunk.js"
+  },
+  {
+    "revision": "e6e0c3a0d9178fa18b34",
+    "url": "/lottery/static/js/runtime~main.50ff0365.js"
+  },
+  {
+    "revision": "40b66466b24b9f674c8dc8b0a67f095e",
+    "url": "/lottery/static/media/magic-wand.40b66466.svg"
+  }
+]);
